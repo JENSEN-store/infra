e82300394c2200cb9e907396fbf4be279adfa1af
Swakshar Dutta <tarzan0607@gmail.com>
2023-09-19 22:14:33 +0200

    changes and added code for prometheus in app js file

--- bestbags-nodejs-ecommerce-master/app.js
+++ bestbags-nodejs-ecommerce-master/app.js
@@ -113,4 +113,33 @@ app.listen(port, () => {
   console.log("Server running at port " + port);
 });
 
+// [swakshar] - -----Prometheus set up ---- line 116 to 143
+
+// Constants
+const PORT = 5002;
+const HOST = '0.0.0.0';
+// App
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+// Probe every 5th second.
+collectDefaultMetrics({ timeout: 5000 });
+const counter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'The total number of processed requests'
+});
+
+const histogram = new client.Histogram({
+  name: 'node_request_duration_seconds',
+  help: 'Histogram for the duration in seconds.',
+  buckets: [1, 2, 5, 6, 10]
+});
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.end(client.register.metrics())
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
 module.exports = app;
