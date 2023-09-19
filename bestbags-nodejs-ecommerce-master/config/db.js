de09062d062fe6500f9cbf40eb6861d05e1ab3f3
unit01 <hanway.tran@gmail.com>
2023-09-19 12:10:49 +0200

    functional

--- bestbags-nodejs-ecommerce-master/config/db.js
+++ bestbags-nodejs-ecommerce-master/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://localhost/bags-ecommerce";
+    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/bags-ecommerce";
     await mongoose
       .connect(uri, {
         useNewUrlParser: true,
